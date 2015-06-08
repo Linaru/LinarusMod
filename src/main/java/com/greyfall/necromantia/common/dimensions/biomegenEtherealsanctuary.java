a0eb02932f6c00d52ba2f6ed5c6d6917acf2d7c3
Linaru <Greyfall@live.co.uk>
2015-06-08 18:50:46 +0100

    some errors fixed.

---
+++ src/main/java/com/greyfall/necromantia/common/dimensions/biomegenEtherealsanctuary.java
@@ -0,0 +1,12 @@
+package com.greyfall.necromantia.common.dimensions;
+
+import net.minecraft.world.biome.BiomeGenBase;
+
+public class BiomegenEtherealsanctuary extends BiomeGenBase{
+
+    public BiomegenEtherealsanctuary(int id)
+    {
+        super(id);
+    }
+
+}
