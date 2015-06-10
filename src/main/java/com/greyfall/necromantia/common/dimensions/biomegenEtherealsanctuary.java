031d9d7e61c125a128f537f6547a6f4023044a64
Lina <Linaru@users.noreply.github.com>
2015-06-10 12:35:49 +0100

    Update biomegenEtherealsanctuary.java

--- src/main/java/com/greyfall/necromantia/common/dimensions/biomegenEtherealsanctuary.java
+++ src/main/java/com/greyfall/necromantia/common/dimensions/biomegenEtherealsanctuary.java
@@ -2,7 +2,7 @@ package com.greyfall.necromantia.common.dimensions;
 
 import net.minecraft.world.biome.BiomeGenBase;
 
-public class BiomegenEtherealsanctuary extends BiomeGenBase{
+public class BiomegenEtherealsanctuary extends BiomeGenBase {
 
     public BiomegenEtherealsanctuary(int id)
     {
