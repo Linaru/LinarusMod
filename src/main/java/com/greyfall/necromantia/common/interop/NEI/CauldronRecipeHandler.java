b650d220757de4654867dcc7fd29aa5a7b99d7e0
Katrina Swales <kat.swales@nekokittygames.com>
2015-06-11 19:10:54 +0100

    Started adding NEI

---
+++ src/main/java/com/greyfall/necromantia/common/interop/NEI/CauldronRecipeHandler.java
@@ -0,0 +1,18 @@
+package com.greyfall.necromantia.common.interop.NEI;
+
+import codechicken.nei.recipe.TemplateRecipeHandler;
+
+/**
+ * Created by Katrina on 11/06/2015.
+ */
+public class CauldronRecipeHandler extends TemplateRecipeHandler{
+    @Override
+    public String getGuiTexture() {
+        return null;
+    }
+
+    @Override
+    public String getRecipeName() {
+        return "necromantia.nei.cauldron";
+    }
+}
