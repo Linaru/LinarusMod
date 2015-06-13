7074440c71ef8441386af39a3ed535f72460e722
Linaru <Greyfall@live.co.uk>
2015-06-13 23:45:09 +0100

    Cauldron Recipies.

--- src/main/java/com/greyfall/necromantia/common/recipes/CauldronRecipes.java
+++ src/main/java/com/greyfall/necromantia/common/recipes/CauldronRecipes.java
@@ -2,6 +2,8 @@ package com.greyfall.necromantia.common.recipes;
 
 import com.greyfall.necromantia.api.crafting.CauldronCrafting;
 import com.greyfall.necromantia.common.Main;
+import com.greyfall.necromantia.common.items.ItemRice;
+import com.greyfall.necromantia.common.items.ModItem;
 import com.greyfall.necromantia.common.items.ModItems;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -11,6 +13,7 @@ import net.minecraftforge.fluids.FluidStack;
 
 /**
  * Created by Katrina on 06/06/2015.
+ * Edited by  by Linaru on 13/06/2015.
  */
 public class CauldronRecipes {
 
@@ -22,8 +25,19 @@ public class CauldronRecipes {
         CauldronCrafting.addNormalRecepie(new ItemStack(Items.apple,1),new FluidStack(FluidRegistry.WATER,250),new ItemStack[] { new ItemStack(ModItems.cookedApple),new ItemStack(ModItems.wax)});
         CauldronCrafting.addNormalRecepie(new ItemStack(Items.golden_apple,1),new FluidStack(FluidRegistry.WATER,250),new ItemStack[] { new ItemStack(ModItems.cookedApple),new ItemStack(ModItems.wax),new ItemStack(Items.gold_nugget,8)});
         CauldronCrafting.addNormalRecepie(new ItemStack(Items.golden_apple,1,1),new FluidStack(FluidRegistry.WATER,250),new ItemStack[] { new ItemStack(ModItems.cookedApple),new ItemStack(ModItems.wax),new ItemStack(Blocks.gold_block,8)});
+        CauldronCrafting.addNormalRecepie(new ItemStack(ModItems.rice,1),new FluidStack(FluidRegistry.WATER,250),new ItemStack[] { new ItemStack(ModItems.riceBall)});
+        CauldronCrafting.addNormalRecepie(new ItemStack(Items.leather,1,1),new FluidStack(FluidRegistry.WATER,500),new ItemStack[] { new ItemStack(ModItems.boiledLeather)});
+        CauldronCrafting.addNormalRecepie(new ItemStack(Items.spider_eye,1,1),new FluidStack(FluidRegistry.WATER,500),new ItemStack[] { new ItemStack(Items.fermented_spider_eye)});
+        CauldronCrafting.addNormalRecepie(new ItemStack(Items.poisonous_potato,1,1),new FluidStack(FluidRegistry.WATER,500),new ItemStack[] { new ItemStack(Items.potato)});
+
+        if (FluidRegistry.isFluidRegistered("acid"))
+            CauldronCrafting.addNormalRecepie(new ItemStack(ModItems.siliconDioxide,1,1),new FluidStack(FluidRegistry.WATER,500),new ItemStack[] { new ItemStack(Items.potato/*Replace this potato with hydrochloric acid.*/)});
+
+
+        /*
         if(FluidRegistry.isFluidRegistered("fuel"))
-            CauldronCrafting.addNormalRecepie(new ItemStack(ModItems.rice),new FluidStack(FluidRegistry.getFluid("fuel"),1000),new ItemStack[]{new ItemStack(ModItems.latex)});
+        CauldronCrafting.addNormalRecepie(new ItemStack(ModItems.rice),new FluidStack(FluidRegistry.getFluid("fuel"),1000),new ItemStack[]{new ItemStack(ModItems.latex)});
         CauldronCrafting.addNormalRecepie("logWood",1,new ItemStack[]{new ItemStack(Blocks.diamond_block)});
+        */
     }
 }
